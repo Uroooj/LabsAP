cf1db795a741618bfd54ff93b60fac0eeae91006
Uroooj <uroojbakht1912@gmail.com>
2016-10-24 16:46:56 +0500

    Add files via upload

---
+++ APlab05.py
@@ -0,0 +1,30 @@
+import os
+import re
+import pprint
+
+
+index ={}
+for root, dirs, files in os.walk('D:\\BsCS 4-C'):
+	for file in files:
+		if file.endswith('.txt'):
+			name = os.path.splitext(file)[0]
+			path = os.path.join(root, file)
+			#print(path)
+			f = open(path, 'r')
+			for line in f:
+				for word in line.split():
+					if word in index:
+						index[word].append(path);
+						
+					else:
+						index[word] = [];
+						index[word].append(path);
+					print(index)
+			f.close()
+
+
+word = input('Enter Word to search: ')
+for word in index:
+	print (index[word])		
+			
+
